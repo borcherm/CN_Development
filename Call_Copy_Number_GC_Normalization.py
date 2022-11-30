0f364c89ca3d2553906a464a6eda765dae4c73c2
Borchers <mb2475@cypress.sgc.loc>
2022-11-30 15:52:15 -0600

    pushing all initial code

---
+++ Call_Copy_Number_GC_Normalization.py
@@ -0,0 +1,160 @@
+import re
+import argparse
+import numpy
+import random
+import math
+import statistics
+import multiprocessing as mp
+import gzip
+from collections import Counter
+
+def get_args():
+  parser = argparse.ArgumentParser(description="Find kmers that uniquely define a satellite array in a genome")
+  parser.add_argument("-r1")
+  parser.add_argument("-r2", required = False)
+  parser.add_argument("-nc1")
+  parser.add_argument("-nc2", required = False)
+  parser.add_argument("-ngcn")
+  parser.add_argument("-ID")
+  parser.add_argument("-gcn")
+  return parser.parse_args()
+
+def takeSecond(elem):
+    return int(elem[1])
+
+
+args = get_args()
+counts_r1 = args.r1
+if args.r2 is not None:
+    counts_r2 = args.r2
+else:
+    counts_r2 = False
+norm_counts1 = args.nc1
+if args.nc2 is not None:
+    norm_counts2 = args.nc2
+else:
+    norm_counts2 = False
+norm_gcn = args.ngcn
+ID = args.ID
+gcn= args.gcn
+
+counts_list = []
+kmer_list = []
+kmer_dic = {}
+
+with open(counts_r1, "r+") as set:
+    while True:
+        l1 = set.readline().strip()
+        kmer = set.readline().strip()
+        if l1 == "":
+            break
+        regex = re.search(r'\>([\S]+)',l1)
+        count = int(regex.group(1))
+        kmer_dic[kmer] = count
+        #kmer_list.append([kmer,count])
+
+if counts_r2 is not False:
+    with open(counts_r2, "r+") as set:
+        while True:
+            l1 = set.readline().strip()
+            kmer = set.readline().strip()
+            if l1 == "":
+                break
+            regex = re.search(r'\>([\S]+)',l1)
+            count = int(regex.group(1))
+            if kmer in kmer_dic:
+                kmer_dic[kmer] = kmer_dic[kmer] + count
+            else:
+                kmer_dic[kmer] = count
+#kmer_list.sort(key=takeSecond, reverse=True)
+#print(kmer_list[0:100])
+#print(kmer_list[-100:])
+
+with open(gcn, "r+") as fh:
+    genomic_copy_number_dic = {}
+    while True:
+        l1 = fh.readline().strip()
+        kmer = fh.readline().strip()
+        if l1 == "":
+            break
+        regex = re.search(r'\>([\S]+)',l1)
+        count = int(regex.group(1))
+        genomic_copy_number_dic[kmer] = count
+
+
+genomic_cn_normalized_counts_dic = {}
+genomic_cn_normalized_counts_list = []
+for kmer in kmer_dic:
+    genomic_cn_normalized_counts_dic[kmer] = kmer_dic[kmer]/genomic_copy_number_dic[kmer]
+    genomic_cn_normalized_counts_list.append(genomic_cn_normalized_counts_dic[kmer])
+
+print(genomic_cn_normalized_counts_dic)
+
+#find median
+median = int(statistics.median_low(genomic_cn_normalized_counts_list))
+print(median)
+
+mean = int(statistics.mean(genomic_cn_normalized_counts_list))
+print(mean)
+
+norm_counts_dic = Counter()
+with open(norm_counts1, "r+") as norm1:
+    while True:
+        l1 = norm1.readline().strip()
+        kmer = norm1.readline().strip()
+        if l1 == "":
+            break
+        regex = re.search(r'\>([\S]+)',l1)
+        count = int(regex.group(1))
+        norm_counts_dic[kmer] += count
+
+if norm_counts2 is not False:
+    with open(norm_counts2, "r+") as norm2:
+        while True:
+            l1 = norm2.readline().strip()
+            kmer = norm2.readline().strip()
+            if l1 == "":
+                break
+            regex = re.search(r'\>([\S]+)',l1)
+            count = int(regex.group(1))
+            norm_counts_dic[kmer] += count
+
+norm_gcn_dic = Counter()
+with open(norm_gcn) as ngcn:
+    while True:
+        l1 = ngcn.readline().strip()
+        kmer = ngcn.readline().strip()
+        if l1 == "":
+            break
+        regex = re.search(r'\>([\S]+)',l1)
+        count = int(regex.group(1))
+        norm_gcn_dic[kmer] += count
+
+norm_counts_adj = Counter()
+for item in norm_counts_dic:
+    norm_counts_adj[item] = float(norm_counts_dic[item]/norm_gcn_dic[item])
+
+
+norm_median = statistics.median(list(norm_counts_adj.values()))
+print(norm_median)
+diploid_norm_median = norm_median/2
+
+ribosome_copy_number = median/norm_median
+print("Median Copy Number" + "\n")
+print(ribosome_copy_number)
+
+mean_copy_number = mean/norm_median
+print("Mean Copy Number:" + "\n")
+print(mean_copy_number)
+
+diploid_ribosome_copy_number = median/diploid_norm_median
+print("Diploid Median Copy Number" + "\n")
+print(diploid_ribosome_copy_number)
+
+diploid_mean_copy_number = mean/diploid_norm_median
+print("Diploid Mean Copy Number:" + "\n")
+print(diploid_mean_copy_number)
+
+with open("Copy_Numbers.tsv", "a+") as summary:
+    #summary.write("Sample" + "\t" + "Median_Haploid_Copy_Number" + "\t" + "Median_Diploid_Copy_Numer" + "\t" + "Mean_Haploid_Copy_Number" + "\t" + "Mean_Diploid_Copy_Number" + "\n")
+    summary.write(ID + "\t" +str(round(ribosome_copy_number)) + "\t" + str(round(diploid_ribosome_copy_number)) + "\t" + str(round(mean_copy_number)) + "\t" + str(round(diploid_mean_copy_number))+"\n")
---
+++ filter_unique_kmers_multithreaded.py
@@ -0,0 +1,212 @@
+import re
+import argparse
+import numpy
+import random
+import math
+import multiprocessing as mp
+import os
+
+def get_args():
+  parser = argparse.ArgumentParser(description="Find kmers that uniquely define a satellite array in a genome")
+  parser.add_argument("-roi", help="fasta file for the region of interest in the genome")
+  #parser.add_argument("-bed_comp", help= "A bed file with the genomic complement of the region of interest (inverse)")
+  parser.add_argument("-k")
+  parser.add_argument("-rest", help="A fasta file for the whole genome but excluding the region of interest")
+  parser.add_argument("-t", help="The number of threads to use")
+  # parser.add_argument("-copycutoff")
+  # parser.add_argument("-less")
+  return parser.parse_args()
+
+args = get_args()
+roi = args.roi
+#bed_comp = args.bed_comp
+k = int(args.k)
+rest = args.rest
+threads= int(args.t)
+# moreless = args.less
+# cutoff = args.copycutoff
+
+def start_process():
+    print('Starting', mp.current_process().name)
+
+def gc_content(sequence):
+    AT = 0
+    GC = 0
+    for character in sequence:
+        if character == "A":
+            AT += 1
+        elif character == "T":
+            AT += 1
+        elif character == "G":
+            GC += 1
+        elif character == "C":
+            GC += 1
+        else:
+            print("Non-uppercase ATGC character detected")
+            break
+    total = GC+AT
+    ratio = GC/total
+    return(round(ratio,2))
+
+def reverse_complement(sequence):
+    new_seq = ""
+    for character in sequence:
+        if character == "A":
+            new_seq += "T"
+        elif character == "T":
+            new_seq += "A"
+        elif character == "G":
+            new_seq += "C"
+        elif character == "C":
+            new_seq += "G"
+        else:
+            print("Non-uppercase ATGC character detected")
+            break
+    reversed_seq = new_seq[::-1]
+    return(reversed_seq)
+
+
+def find_unique(assembly):
+    print("Identifying feature-specific kmers")
+    with open(assembly, "r+") as ref:
+        for line in ref:
+            seq = line.strip()
+            if seq[0] == ">":
+                continue
+            for n in range(len(seq)-k+1):
+                kmer = seq[n:n+k]
+                kmer_revcomp = reverse_complement(kmer)
+                if kmer in kmer_dic:
+                    #print(kmer)
+                    del kmer_dic[kmer]
+                elif kmer_revcomp in kmer_dic:
+                    del kmer_dic[kmer_revcomp]
+    return(kmer_dic)
+
+
+
+
+print("Generating array kmers")
+with open(roi, "r+") as fh:
+    kmer_dic = {}
+    while True:
+        seqname = fh.readline().strip()
+        if seqname == '':
+            break
+        seq = fh.readline().strip()
+        #seqID = re.search(r'\>([\S]+)\:[\S]+',seqname)
+        #chrID = seqID.group(1)
+        Array_GC = gc_content(seq)
+        for n in range(len(seq)-k+1):
+            kmer = seq[n:n+k]
+            if kmer in kmer_dic:
+                kmer_dic[kmer] += 1
+            else:
+                kmer_dic[kmer] = 1
+
+
+#combine any kmers from the array set that are reverse complements
+print("The length before deleting duplicates:")
+print(len(kmer_dic))
+duplicate_rev_comp_list = []
+duplicates = False
+for kmer in kmer_dic:
+    kmer_revcomp = reverse_complement(kmer)
+    if kmer_revcomp in kmer_dic:
+        duplicates = True
+        if kmer not in duplicate_rev_comp_list:
+            kmer_dic[kmer] += kmer_dic[kmer_revcomp]
+            duplicate_rev_comp_list.append(kmer_revcomp)
+if duplicates == True:
+    print("There are reverse complement kmers present in the genomic sequence for this feature")
+for entry in duplicate_rev_comp_list:
+    del kmer_dic[entry]
+print("The length after deleting duplicates:")
+print(len(kmer_dic))
+
+
+
+# below_cutoff = []
+# if moreless == "more":
+#     for item in kmer_dic:
+#         if int(kmer_dic[item]) < int(cutoff):
+#             below_cutoff.append(item)
+# elif moreless == "less":
+#     for item in kmer_dic:
+#         if int(kmer_dic[item]) > int(cutoff):
+#             below_cutoff.append(item)
+# for kmer in below_cutoff:
+#     del kmer_dic[kmer]
+
+
+
+#split the assembly into single chromosomes, loop over each chromosome as a separate file
+file_list = []
+with open(rest, "r+") as ref:
+    while True:
+        seqname = ref.readline().strip()
+        if seqname == "":
+            break
+        seq = ref.readline().strip()
+        seqID = re.search(r'\>([\S]+)',seqname)
+        if seqID.group(1) == False:
+            print(seqname)
+        chrmID = seqID.group(1)
+        file_string = chrmID + "_assembly_split.fa"
+        file_list.append(file_string)
+        out = open(file_string,"w+")
+        out.write(seqname)
+        out.write("\n")
+        out.write(seq)
+        out.write("\n")
+
+
+if __name__ == '__main__':
+    pool = mp.Pool(processes= threads,initializer=start_process)
+    pool_outputs1 = pool.map(find_unique, [fh for fh in file_list])
+    pool.close()
+    pool.join()
+
+print("The number of pool outputs is:")
+print(len(pool_outputs1))
+num_outputs = len(pool_outputs1)
+
+kmer_dic_list=numpy.empty(num_outputs,dtype='object')
+for i in range(num_outputs):
+    kmer_dic_list[i] = pool_outputs1[i]
+
+
+#merge the 23+ output dictionaries, removing keys which were not present in any single output
+#these represent non unique kmers
+for i in range(len(kmer_dic_list)):
+    if i == 0:
+        merged_kmer_dic = kmer_dic_list[i]
+    else:
+        #To allow myself to delete kmers during loop, I have to delete from a copy dictionary
+        #This dicitonary becomes the merged dic at the end
+        for item in merged_kmer_dic.copy():
+            if item in kmer_dic_list[i].keys():
+                continue
+            elif kmer_revcomp in kmer_dic_list[i].keys():
+                continue
+            else:
+                del merged_kmer_dic[item]
+
+print(merged_kmer_dic)
+
+
+# graph = open("kmer_frequency_x_array.tsv","a+")
+# for entry in kmer_dic:
+#     graph.write(entry+"\t"+str(kmer_dic[entry]))
+#     graph.write("\n")
+
+set = open("region_specific_kmers.fa","w+")
+for entry in merged_kmer_dic:
+    set.write(">" + str(merged_kmer_dic[entry]))
+    set.write("\n")
+    set.write(entry)
+    set.write("\n")
+set.close()
+
+for file in file_list:
+    os.remove(file)
